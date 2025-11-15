f70139317379f194e48c63cbde629d927e75ddda
Nijiiiiiii <n.martinet@univ-lyon2.fr>
2025-11-15 12:01:12 +0100

    Projet V1 OK
    Les TD3 et 5 ont été fait entièrement, le code à été revu, formater, corriger etc...
    Un base test à été crée sur les post les plus récents (aucun sujet en particulier).

--- ProjetV1(TD3-5)/Author.py
+++ ProjetV1(TD3-5)/Author.py
@@ -1,25 +1,31 @@
+# Création de la class des auteurs
 class Author:
-    #La définition de la class commence tjs par un constructeur
+    # Constructeur de la class
     def __init__(self, name):
-        self.name=name
-        self.ndoc=0
-        self.production={} #aura des obj de class Document
+        self.name = name  # Nom de l'auteur
+        self.ndoc = 0  # Nombre de documents produits par l'auteur
+        self.production = {}  # Dictionnaire des documents produits par l'auteur
 
+    # Fonction add() qui permet d'ajouter un document à la production de l'auteur
     def add(self, doc_id, document):
-        self.production[doc_id]=document
+        self.production[doc_id] = document
         self.ndoc += 1
 
+    # Fonction str() pour afficher les informations de l'auteur via print(auteur)
     def __str__(self):
         return f"Auteur: {self.name} - {self.ndoc} document(s)"
 
+    # Fonction repr() pour faire une chaîne de caractères qui sert de représentation à la class
     def __repr__(self):
         return f"Auteur(name={self.name}, ndoc={self.ndoc})"
-    
+
+    # Fonction get_taille_moyenne_documents() qui retourne la taille moyenne (nombre de caractère moyen) des documents produits par l'auteur
     def get_taille_moyenne_documents(self):
-        if self.ndoc==0:
+        if self.ndoc == 0:
             return 0
         total_cara = sum(len(doc.texte) for doc in self.production.values())
-        return total_cara/self.ndoc
-    
+        return total_cara / self.ndoc
+
+    # Fonction get_nombre_documents() qui retourne le nombre de documents produits par l'auteur
     def get_nombre_documents(self):
         return self.ndoc
--- ProjetV1(TD3-5)/squellette.py
+++ ProjetV1(TD3-5)/squellette.py
@@ -4,49 +4,78 @@ import xmltodict
 import pandas as pd
 import os
 from Corpus import Corpus
-from Document import Document, RedditDocument, ArxivDocument, DocumentGenerator
+from Document import DocumentGenerator
 
-docs = Corpus("bl")
+docs = Corpus("test")
 
 if os.path.exists("test.csv"):
     docs.load("test")
-else :
-    #Se connecter sur Reddit
-    reddit = praw.Reddit(client_id='chp-OSvcT2YNGYMdLRLTdw', client_secret='JDBMQbv6014x63VLUFbUb4dDX0-mTQ', user_agent='Blbl')
+else:
+    # Se connecter sur Reddit
+    reddit = praw.Reddit(
+        client_id="chp-OSvcT2YNGYMdLRLTdw",
+        client_secret="JDBMQbv6014x63VLUFbUb4dDX0-mTQ",
+        user_agent="Blbl",
+    )
 
-    #On va charger le CORPS de chaque subreddit en lien avec le machinLearning (en vrai que 10 c'est bien)
-    hot_posts  = reddit.subreddit('all').hot(limit=100)
-    for post in hot_posts:        
-        texte=post.selftext.replace("\n"," ")
-        titre=post.title
-        date=post.created
-        url=post.url
-        auteur=post.author
+    # On va charger le CORPS de chaque subreddit en lien avec le machinLearning (en vrai que 10 c'est bien)
+    hot_posts = reddit.subreddit("all").hot(limit=5000)
+    for post in hot_posts:
+        texte = post.selftext.replace("\n", " ")
+        titre = post.title
+        # Transforme la date en format date
+        date = pd.to_datetime(post.created, unit="s").date()
+        url = post.url
+        auteur = post.author
         nbcomment = post.num_comments
+        # Insert le document au corpus
         docs.add_document(DocumentGenerator.factory("Reddit", titre, auteur, date, url, texte, nbcomment))
-    
-    url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=2'
+
+    url = "http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=5000"
     requete = urllib.request.urlopen(url)
-    parsed=xmltodict.parse(requete.read().decode('utf-8'))
-    for j in range(0,len(parsed['feed']['entry'])):
-        texte=parsed['feed']['entry'][j]['summary'].replace("\n"," ")
-        auteur=parsed['feed']['entry'][j]['author'][0]['name']
-        coauteur = ""
-        for i in range(1,len(parsed['feed']['entry'][j]['author'])):
-            coauteur+=parsed['feed']['entry'][j]['author'][i]['name'] + "//"
-        titre=parsed['feed']['entry'][j]['title']
-        date=parsed['feed']['entry'][j]['published']
-        url=parsed['feed']['entry'][j]['id']
-        docs.add_document(DocumentGenerator.factory("Arxiv", titre, auteur, date, url, texte, coauteur))
-    
-    
+    parsed = xmltodict.parse(requete.read().decode("utf-8"))
+    for j in range(0, len(parsed["feed"]["entry"])):
+        texte = parsed["feed"]["entry"][j]["summary"].replace("\n", " ")
+        
+        # Vu qu'il existe des co-auteur, on crée une variable avec la liste des auteurs
+        auteur_list = parsed["feed"]["entry"][j]["author"]
+
+        # Si on obtient une liste => Liste d'auteur avec plusieurs auteurs
+        if type(auteur_list) == list:
+            auteur = parsed["feed"]["entry"][j]["author"][0]["name"]
+            coauteur = ""
+            for i in range(1, len(auteur_list)):
+                coauteur += parsed["feed"]["entry"][j]["author"][i]["name"] + " et "
+            coauteur = coauteur[:-4]  # Enlever le dernier " et "
+        # Sinon -> Un seul auteur
+        else:
+            auteur = parsed["feed"]["entry"][j]["author"]["name"]
+            coauteur = "Aucun"
+        
+        titre = parsed["feed"]["entry"][j]["title"]
+        # Transforme la date en format date
+        date = pd.to_datetime(parsed["feed"]["entry"][j]["published"]).date()
+        url = parsed["feed"]["entry"][j]["id"]
+        # Insert le document au corpus
+        docs.add_document(
+            DocumentGenerator.factory(
+                "Arxiv", titre, auteur, date, url, texte, coauteur
+            )
+        )
+
+    # On repasse dans tout les documents
+    # Etant données que nous allons supprimer des documents nous sommes obliger de passer par une copie de la liste des id
     for i in docs.id2doc.copy():
-        if type(docs.id2doc[i].texte) is not str :
+        # Si le texte n'est pas de type string on supprime le document
+        if type(docs.id2doc[i].texte) is not str:
             docs.rem_document(i)
-        else : 
+        else:
             nbcara = len(str(docs.id2doc[i].texte))
-            if nbcara<20:
+            # Si le document n'a pas assez de caractère on le supprime
+            if nbcara < 20:
                 docs.rem_document(i)
 
+    # On enregiste le document crée
     docs.save("test")
+
 print(docs)
